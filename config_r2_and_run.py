eb9bdcbd1ec2919ff5acd5332d12a88fcf6551ea
chrisc36 <chrisc@allenai.org>
2024-03-08 00:36:30 -0800

    new experiment

---
+++ config_r2_and_run.py
@@ -0,0 +1,18 @@
+import sys
+from os import mkdir, environ
+from os.path import expanduser, join
+from subprocess import run
+
+
+def main():
+    aws_dir = expanduser("~/.aws")
+    print(f"Setting up AWS at {aws_dir}")
+    mkdir(aws_dir)
+    with open(join(aws_dir, "credentials"), "w") as f:
+        f.write(environ["R2_CREDENTIALS"])
+    print("Starting torchrun")
+    run(list(sys.argv[1:]))
+
+
+if __name__ == '__main__':
+    main()
